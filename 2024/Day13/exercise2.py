37668469407e55d5af7c74778294ce3c9ad92357
Laura Martínez Sanahuja <laura.martinez@king.com>
2024-12-09 14:46:15 +0100

    Add template for 2024 days

---
+++ 2024/Day13/exercise2.py
@@ -0,0 +1,11 @@
+data_path = "2024/Day13/"
+data_file = "example.txt"
+# data_file = "data.txt"
+
+result: int = 0
+
+with open(data_path + "/" + data_file, "r") as file:
+    for line in file:
+        pass  # LOGIC HERE
+
+print(f'Result: {result}')
