95e124cb339715ee615b945d264c2343472b83d8
Laura Martínez Sanahuja <laura.martinez@king.com>
2024-12-12 07:29:19 +0100

    Day 11

--- 2024/Day11/exercise1.py
+++ 2024/Day11/exercise1.py
@@ -1,11 +1,35 @@
 data_path = "2024/Day11/"
 data_file = "example.txt"
-# data_file = "data.txt"
+data_file = "data.txt"
 
 result: int = 0
+stones = []
+
+
+def do_blink():
+    global stones
+    result_stones = []
+    for stone in stones:
+        if stone == "0":
+            result_stones.append("1")
+        elif len(stone) % 2 == 0:
+            result_stones.append(str(int(stone[0 : int(len(stone) / 2)])))
+            result_stones.append(str(int(stone[int(len(stone) / 2) : int(len(stone))])))
+        else:
+            result_stones.append(str(int(stone) * 2024))
+    stones = result_stones
+
 
 with open(data_path + "/" + data_file, "r") as file:
     for line in file:
-        pass  # LOGIC HERE
+        stones = line.split(" ")
 
+print(stones)
+
+BLINKS = 25
+for blink in range(BLINKS):
+    #print(f"BLINK {blink+1}")
+    do_blink()
+
+result = len(stones)
 print(f'Result: {result}')
--- 2024/Day11/exercise2.py
+++ 2024/Day11/exercise2.py
@@ -1,11 +1,77 @@
 data_path = "2024/Day11/"
 data_file = "example.txt"
-# data_file = "data.txt"
+data_file = "data.txt"
 
 result: int = 0
+stones = {}
+to_add = {}
+to_remove = {}
+
+
+def stone_to_add(stone, qty):
+    global to_add
+    if stone not in to_add:
+        to_add[stone] = 0
+    to_add[stone] = to_add[stone] + qty
+
+
+def stone_to_remove(stone, qty):
+    global to_remove
+    if stone not in to_remove:
+        to_remove[stone] = 0
+    to_remove[stone] = to_remove[stone] + qty
+
+
+def add_stones():
+    global to_add
+    for stone in to_add:
+        if stone not in stones:
+            stones[stone] = 0
+        stones[stone] = stones[stone] + to_add[stone]
+    to_add = {}
+
+
+def remove_stones():
+    global to_remove
+    for stone in to_remove:
+        stones[stone] = stones[stone] - to_remove[stone]
+        if stones[stone] == 0:
+            stones.pop(stone)
+    to_remove = {}
+
+
+def do_blink():
+    global stones, to_add, to_remove
+    for stone in stones:
+        stone_to_remove(stone, stones[stone])
+        if stone == 0:
+            stone_to_add(1, stones[stone])
+        elif len(str(stone)) % 2 == 0:
+            stone_to_add(int(str(stone)[0 : int(len(str(stone)) / 2)]), stones[stone])
+            stone_to_add(
+                int(str(stone)[int(len(str(stone)) / 2) : int(len(str(stone)))]), stones[stone]
+            )
+        else:
+            stone_to_add(stone * 2024, stones[stone])
+
+    add_stones()
+    remove_stones()
+
+
+stone_values = []
 
 with open(data_path + "/" + data_file, "r") as file:
     for line in file:
-        pass  # LOGIC HERE
+        stone_values = line.split(" ")
 
+for stone_value in stone_values:
+    stones[int(stone_value)] = 1
+
+
+BLINKS = 75
+for blink in range(BLINKS):
+    #print(f"BLINK {blink+1}")
+    do_blink()
+
+result = sum(stones.values())
 print(f'Result: {result}')
